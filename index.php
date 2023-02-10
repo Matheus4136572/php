f5e47de00fa1e195f32b02ac27d3c3977f7fb29d
Matheus4136572 <93297774+Matheus4136572@users.noreply.github.com>
2023-02-10 17:51:58 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo ('Funciona')
+<div class="Form-group">
+    <div class="form">
+        <input type="text" name="nome" id="">
+        <input type="email" name="email" id="">
+        <input type="password" name="senha" id="">
+    </div>
+</div>
+?>

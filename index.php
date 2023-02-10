9f31245b93dca8dbfd6ca0cdccf54a7c3c2d226d
Matheus4136572 <93297774+Matheus4136572@users.noreply.github.com>
2023-02-10 17:54:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
 echo ('Funciona')
-<div class="Form-group">
-    <div class="form">
-        <input type="text" name="nome" id="">
-        <input type="email" name="email" id="">
-        <input type="password" name="senha" id="">
-    </div>
-</div>
 ?>
